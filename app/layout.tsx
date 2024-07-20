c5352dcf80a9ad291ef4bfedd58258514a4baf01
Jarren San Jose <sanjose.jarren@gmail.com>
2024-07-20 16:49:35 -0700

    not health advice

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'not health advice',
+    template: '%s | not health advice',
   },
-  description: 'This is my portfolio.',
+  description: 'This is not health advice.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'not health advice',
+    description: 'This is not health advice.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'not health advice',
     locale: 'en_US',
     type: 'website',
   },
