126637e452aae0f3466b8828d7f616cb14e8cc6e
Jarren San Jose <sanjose.jarren@gmail.com>
2024-07-25 14:52:12 -0700

    edit home page text to include disclaimer

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ export default function Page() {
         not health advice
       </h1>
       <p className="mb-4">
-        {`I'm a software engineer turned trader. My focus has shifted from my career to my health. I've always been a hypochondriac though and have OCD. As a trader, I rely on my mental (headspace) and physical well-being. If I don't feel well, I definitely get punished by the markets. These notes are references for myself and are not medical or financial advice. I'm not a medical professional or financial advisor.`}
+        {`I'm a software engineer turned trader. My focus has shifted from my career to my health.`}
+      </p>
+      <p className="mb-4">
+        {`These notes are references for myself and are not medical or financial advice. I'm not a medical professional or financial advisor. The information presented may be biased and inaccurate. Please do your own research.`}
       </p>
       <div className="my-8">
         <BlogPosts />
